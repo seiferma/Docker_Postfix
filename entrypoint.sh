768d95b063e66a8238b3e8e32603ab063113b99d
Stephan Seifermann <seiferma@users.noreply.github.com>
2022-12-27 16:26:17 +0100

    fix permissions on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,12 @@
 set -e
 
 if [ "$1" == "postfix" ]; then
-    ls -la /
-    ls -la /postfix-cfg
     cp -rf "$CFG_DIR"/* /etc/postfix/
     find /etc/postfix/hashed -type f -regex '.*/[^.]\+' -maxdepth 1 -exec postmap {} \;
     chown root:root -R /etc/postfix/
     find /etc/postfix/ -type d -exec chmod 0750 {} \;
     find /etc/postfix/ -type f -exec chmod 0640 {} \;
+    /usr/sbin/postfix -c /etc/postfix set-permissions
     exec /usr/sbin/postfix -c /etc/postfix start-fg
 else
     exec "$@"
